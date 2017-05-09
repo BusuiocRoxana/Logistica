cca7aa0bdf39dc257540afacaaa5ff7659e2d230
Roxana <busuioc.roxana@gmail.com>
2017-05-09 23:01:08 +0300

    - drawables folder

---
+++ app/src/main/java/disertatie/com/disertatie/Database/DatabaseHelper.java
@@ -0,0 +1,95 @@
+package disertatie.com.disertatie.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Roxana on 5/9/2017.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String SPACE = " ";
+
+    public static final String DATABASE_NAME = "LOGISTICA.db";
+    public static final String TABLE_COMPANIE = "COMPANIE";
+
+    public static final String COLUMN_COD_COMPANIE = "COD_COMPANIE";
+    public static final String COLUMN_DENUMIRE_COMPANIE = "DENUMIRE_COMPANIE";
+    public static final String COLUMN_NR_INREG_RC = "NR_INREG_RC";
+    public static final String COLUMN_EMAIL_COMPANIE = "EMAIL_COMPANIE";
+    public static final String COLUMN_COD_ADRESA = "COD_ADRESA";
+    public static final String COLUMN_TELEFON_COMPANIE = "TELEFON_COMPANIE";
+
+
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table"
+                + SPACE + TABLE_COMPANIE +
+                "("
+                + COLUMN_COD_COMPANIE + SPACE
+                + "integer primary key,"
+                + COLUMN_DENUMIRE_COMPANIE + SPACE +
+                "text not null," +
+                COLUMN_NR_INREG_RC + SPACE +
+                "text not null," +
+                COLUMN_EMAIL_COMPANIE + SPACE +
+                "text," +
+                COLUMN_COD_ADRESA + SPACE +
+                "text," +
+                COLUMN_TELEFON_COMPANIE + SPACE +
+                "text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS"+SPACE+TABLE_COMPANIE);
+        onCreate(db);
+    }
+
+    public boolean insertCompanie(String denumire_companie, String nr_inreg_rc, String email, int cod_adresa,String telefon) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_COD_ADRESA, denumire_companie);
+        contentValues.put(COLUMN_NR_INREG_RC, nr_inreg_rc);
+        contentValues.put(COLUMN_EMAIL_COMPANIE, email);
+        contentValues.put(COLUMN_COD_ADRESA, cod_adresa);
+        contentValues.put(COLUMN_TELEFON_COMPANIE, telefon);
+        db.insert(TABLE_COMPANIE, null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from"+SPACE+TABLE_COMPANIE+SPACE+"where"+SPACE+COLUMN_COD_COMPANIE+"="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, TABLE_COMPANIE);
+        return numRows;
+    }
+
+    public boolean updateCompanie (int cod_companie, String denumire_companie, String nr_inreg_rc, String email, int cod_adresa,String telefon) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_COD_ADRESA, denumire_companie);
+        contentValues.put(COLUMN_NR_INREG_RC, nr_inreg_rc);
+        contentValues.put(COLUMN_EMAIL_COMPANIE, email);
+        contentValues.put(COLUMN_COD_ADRESA, cod_adresa);
+        contentValues.put(COLUMN_TELEFON_COMPANIE, telefon);
+        db.update(TABLE_COMPANIE, contentValues,COLUMN_COD_COMPANIE+"= ? ", new String[]{Integer.toString(cod_companie)});
+        return true;
+    }
+
+}
--- app/src/main/java/disertatie/com/disertatie/activities/DateInterneActivity.java
+++ app/src/main/java/disertatie/com/disertatie/activities/DateInterneActivity.java
@@ -1,15 +1,27 @@
 package disertatie.com.disertatie.activities;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import disertatie.com.disertatie.Database.DatabaseHelper;
 import disertatie.com.disertatie.R;
 
 public class DateInterneActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private DatabaseHelper databaseHelper;
+    int id_To_Update = 0;
+
+    private EditText etDenumireCompanie;
+    private EditText etNrInregRC;
+    private EditText etEmailCompanie;
+    private EditText etTelefonCompanie;
+    private EditText etCodAdresaCompanie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +41,55 @@ public class DateInterneActivity extends AppCompatActivity {
             tv.setText(R.string.pagina_date_interne);
         }
 
+        etDenumireCompanie = (EditText) findViewById(R.id.etDenumireCompanie);
+        etNrInregRC = (EditText) findViewById(R.id.etNrInregistrareRC);
+        etEmailCompanie = (EditText) findViewById(R.id.etEmailCompanie);
+        etTelefonCompanie = (EditText) findViewById(R.id.etTelefonCompanie);
+        etCodAdresaCompanie = (EditText) findViewById(R.id.etAdresaCompanie);
+
+        databaseHelper = new DatabaseHelper(this);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            int Value = extras.getInt("id");
+
+            if (Value > 0) {
+                //means this is the view part not the add contact part.
+                Cursor rs = databaseHelper.getData(Value);
+                id_To_Update = Value;
+                rs.moveToFirst();
+
+                String denumire = rs.getString(rs.getColumnIndex(DatabaseHelper.COLUMN_DENUMIRE_COMPANIE));
+                String nrInregRC = rs.getString(rs.getColumnIndex(DatabaseHelper.COLUMN_NR_INREG_RC));
+                String adresa = rs.getString(rs.getColumnIndex(DatabaseHelper.COLUMN_COD_ADRESA));
+                String telefon = rs.getString(rs.getColumnIndex(DatabaseHelper.COLUMN_TELEFON_COMPANIE));
+                String email = rs.getString(rs.getColumnIndex(DatabaseHelper.COLUMN_EMAIL_COMPANIE));
+
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+
+                etDenumireCompanie.setText((CharSequence) denumire);
+                etDenumireCompanie.setFocusable(false);
+                etDenumireCompanie.setClickable(false);
+
+                etTelefonCompanie.setText((CharSequence) telefon);
+                etTelefonCompanie.setFocusable(false);
+                etTelefonCompanie.setClickable(false);
+
+                etEmailCompanie.setText((CharSequence) email);
+                etEmailCompanie.setFocusable(false);
+                etEmailCompanie.setClickable(false);
+
+                etCodAdresaCompanie.setText((CharSequence) adresa);
+                etCodAdresaCompanie.setFocusable(false);
+                etCodAdresaCompanie.setClickable(false);
+
+                etTelefonCompanie.setText((CharSequence) telefon);
+                etTelefonCompanie.setFocusable(false);
+                etTelefonCompanie.setClickable(false);
+            }
+
+        }
     }
 }
