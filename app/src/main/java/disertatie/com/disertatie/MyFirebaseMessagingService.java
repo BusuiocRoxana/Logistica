a1b27bb210e2e53069f24f78007105d43f70fbff
roxana <busuioc.roxana@gmail.com>
2017-05-17 00:08:19 +0300

    basic handling of messages

--- app/src/main/java/disertatie/com/disertatie/MyFirebaseMessagingService.java
+++ app/src/main/java/disertatie/com/disertatie/MyFirebaseMessagingService.java
@@ -1,12 +1,20 @@
 package disertatie.com.disertatie;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by george on 16.05.2017.
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
+    private static final String TAG="Logistica";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.i(TAG,"Message received : "+remoteMessage.toString());
+    }
 
 }
